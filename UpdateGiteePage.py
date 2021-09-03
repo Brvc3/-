7edb83150ab0f827969e52a33f193421ed210ad5
Bruce <84448237+Brvc3@users.noreply.github.com>
2021-09-03 14:21:17 +0800

    Update UpdateGiteePage.py

--- UpdateGiteePage.py
+++ UpdateGiteePage.py
@@ -13,9 +13,9 @@ time.sleep(2)
 
 # 输入账号--通过html的id属性定位输入位置--改为你的账号
 user_login = driver.find_element_by_id('user_login')
-user_login.send_keys("15851688868")
+user_login.send_keys("--")
 # 输入密码--通过html的id属性定位输入位置--改为你的密码
-driver.find_element_by_id('user_password').send_keys("wjc000925")
+driver.find_element_by_id('user_password').send_keys("--")
 # 点击登录按钮--通过xpath确定点击位置
 driver.find_element_by_xpath(
     '/html/body/div[2]/div[2]/div[1]/div/div[2]/div/form[1]/div[2]/div/div/div[4]/input').click()
@@ -23,7 +23,7 @@ driver.find_element_by_xpath(
 time.sleep(2)
 
 # 切换到gitee pages界面--改为you_gitee_id
-driver.get('https://gitee.com/twzx/WuYi000925.github.io/pages')
+driver.get('https://gitee.com/--/pages')
 # 点击更新按钮--通过xpath确定点击位置
 driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[2]/div[1]/form/div[7]').click()
 # 确认更新提示框--这个函数的作用是确认提示框
